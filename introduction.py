7efab8dfa6743dc5247845ada7f187fa659a181c
tutty.ben <tutty.ben@awatapu.school.nz>
2021-11-03 14:36:59 +1300

    Initial commit

---
+++ introduction.py
@@ -0,0 +1,18 @@
+#Functions
+def instructions():
+    print("***         Welcome to this Gaming Quiz         ***")
+    print("*** Were the success rate is extremely pitiful! ***")
+    print("***     To play you must answer 5 questions     ***")
+    print("***           All are multiple choice           ***")
+    print("***    Incorrect answers lead to punishment     ***")
+    print("***         Correct answers are rewarded        ***")
+    print("***     Score higher to earn a better grade     ***")
+
+#Main code
+heard_of = input("Have you heard of this quiz before?")
+if heard_of == "no":
+    instructions()
+elif heard_of == "yes":
+    print("Well we better get started then!")
+else:
+    print("Please enter yes or no!")
