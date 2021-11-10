5c378139e6adf751286c1ea268bb5b2c5a41ccad
tutty.ben <tutty.ben@awatapu.school.nz>
2021-11-11 09:13:57 +1300

    Questions Complete

---
+++ Questions_ask.py
@@ -0,0 +1,46 @@
+score = 0
+
+quest_ask1 = input("What year was the full version of Minecraft released?")
+
+if quest_ask1 == "2011":
+    print("Correct!")
+    score += 1
+else:
+    print("Sorry that isn't correct!")
+
+quest_ask2 = input("How many games does Sonic the hedgehog appear in?")
+
+if quest_ask2 == "90":
+    print("Correct!")
+    score += 1
+else:
+    print("Nope that's incorrect")
+
+quest_ask3 = input("What year did Roblox release?")
+
+if quest_ask3 == "2006":
+    print("That is correct!")
+    score += 1
+else:
+   print("Nope Incorrect!")
+
+quest_ask4 = input("What was the most popular Roblox game in 2021?")
+
+if quest_ask4 == "Piggy":
+    print("Absolutely correct!")
+    score += 1
+else:
+    print("Oops, That's not right!")
+
+quest_ask5 = input("How many copies of minecraft were sold in 2021?")
+
+if quest_ask5.strip() == "200million":
+    print("100% Correct!")
+    score += 1
+else:
+    print("Sadly, That's incorrect")
+
+
+
+
+
