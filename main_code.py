efcc4d3e5b396275920e5c2d05a44fbe22042685
tutty.ben <tutty.ben@awatapu.school.nz>
2021-11-11 09:57:21 +1300

    Main Code with Grading

--- main_code.py
+++ main_code.py
@@ -62,3 +62,19 @@ if quest_ask5.strip() == "200million":
     score += 1
 else:
     print("Sadly, That's incorrect")
+
+#Figure out the users grading
+if score == 0:
+    grade = "F"
+    print("Damn that's not good, Oh well there's always next time!")
+elif score == 1 or score == 2:
+    grade = "B"
+    print("Oh no! So close!")
+elif score == 3 or score == 4:
+    grade = "A"
+    print("Great job You win! Now try to get a better score!")
+else:
+    grade = "S"
+    print("Wow amazing! A perfect score!")
+# Tell the user their final grade
+print("Your grade is {}".format(grade))
